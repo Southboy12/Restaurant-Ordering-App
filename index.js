3a5c353f087b7a6ab602084c2499c6bd351a9389
southboy12 <menkachi85@gmail.com>
2024-10-29 22:59:38 +0100

    removed index and added data

--- index.js
+++ index.js
@@ -5,19 +5,19 @@ const menuEl = document.querySelector('#menus')
 
 function getHtml() {
     
-    const allMenu = menuArray.map((menu, index) => {
+    const allMenu = menuArray.map(menu => {
         return `
-            <div class="menu-details">
+            <div class="menu-details" data-key="${menu.id}">
                 <p class="menu-emoji">${menu.emoji}</p>
                 <div class="menu-info">
                     <h3 class="menu-title">${menu.name}</h3>
                     <p class="menu-description">${menu.ingredients}</p>
-                    <p class="menu-price">$${menu.price}</p>
+                    <p class="price">$${menu.price}</p>
                 </div>
-                <button class="add">+</button>
+                <button class="add" data-key="${menu.id}">+</button>
             </div>
         `
-    })
+    }).join('')
     return allMenu
 }
 
