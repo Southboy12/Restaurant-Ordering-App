a9a479aedf3a655fd6c18664e185182fc86ba963
southboy12 <menkachi85@gmail.com>
2024-10-31 07:16:21 +0100

    render from javascript

--- index.js
+++ index.js
@@ -2,22 +2,16 @@ import { menuArray  } from "./data";
 
 
 const menuEl = document.querySelector('#menus')
-
 const preCheckout = document.querySelector('#pre-checkout')
 
 
 document.addEventListener('click', function(e) {
     if (e.target.dataset.key) {
         handleAddMenu(e.target.dataset.key)
-    }
-    
-    
+    }    
 })
 
-
-let isPreCheckoutrendered = false
 let orderLIst = []
-
 
 function handleAddMenu(btnId) {
 
@@ -30,60 +24,32 @@ function handleAddMenu(btnId) {
         price: selectedObj.price,
         id: selectedObj.id
     })    
-    // console.log(orderLIst);
-    
+
     const orderHtml = orderLIst.map(order => {
-        return order
-    })
-    console.log(orderHtml);
+        return `
+        <div class="order-item">
+            <h3 class="order-title">${order.name}</h3> 
+            <button class="remove-btn">remove</button>
+            <p class="price">$${order.price}</p>
+        </div>
+        `        
+    }).join('')
 
+
+    preCheckout.innerHTML = `
+        <h3 class="checkout-title">Your order</h3>
+        <div class="order-entry margin-container" id="order-item">
+            ${orderHtml}
+        </div>
+    
+        <div class="divider margin-container"></div>
+        <div class="total-section order-item margin-container">
+            <h3 class="total order-title">Total price:</h3>
+            <p class="price">$12</p>
+        </div>
+        <button class="purchase-btn" id="purchase-btn">Complete order</button>
+    `
 }
-
-
-
-
-
-// function handleAddMenu(btnId) {
-
-//     menuArray.forEach(menu => {
-        
-//         if (menu.id === Number(btnId)) {
-
-            
-
-//             if (!isPreCheckoutrendered) {                
-//                 preCheckout.style.visibility = 'visible'
-
-//                 preCheckout.innerHTML = `
-//                     <h3 class="checkout-title">Your order</h3>
-//                     <div class="order-item margin-container" id="order-item">
-//                         <h3 class="order-title">${menu.name}</h3> 
-//                         <button class="remove-btn">remove</button>
-//                         <p class="price">$${menu.price}</p>
-//                     </div>
-                
-//                     <div class="divider margin-container"></div>
-//                     <div class="total-section order-item margin-container">
-//                         <h3 class="total order-title">Total price:</h3>
-//                         <p class="price">$${menu.price}</p>
-//                     </div>
-//                     <button class="purchase-btn" id="purchase-btn">Complete order</button>
-//                 `
-//                 isPreCheckoutrendered = true
-//             }
-//             else {
-//                 const orderItemEl = document.querySelector('#order-item');
-//                 orderItemEl.innerHTML += `
-//                     <h3 class="order-title">${menu.name}</h3> 
-//                     <button class="remove-btn">remove</button>
-//                     <p class="price">$${menu.price}</p>
-//                 `
-//             }
-//         }
-        
-//     });      
-// }
-
 
 function getMenuHtml() {
     
@@ -110,16 +76,6 @@ function getMenuHtml() {
 
 function render() {
     menuEl.innerHTML = getMenuHtml()
-
-
-    // const addBtns = document.querySelectorAll('.add-btn')
-    // addBtns.forEach(addBtn => addBtn.addEventListener('click', handleAddMenu));
-    
-    // document.addEventListener('click', function(e) {
-
-    //     handleAddMenu(e.target.dataset.key)
-        
-    // })
 }
 
 render()
